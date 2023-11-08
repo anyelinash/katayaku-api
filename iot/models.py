1a5d57ac7dba18bb0a6d40e8bbcc047e45c138aa
anyelinash <anyelina.sosa@tecsup.edu.pe>
2023-11-07 20:19:15 -0500

    modelos

--- iot/models.py
+++ iot/models.py
@@ -1,3 +1,96 @@
 from django.db import models
+from auth.models import Empresa
 
-# Create your models here.
+# Módulos
+class Modulo(models.Model):
+    cod_modulo = models.AutoField(primary_key=True)
+    codigo_emp = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=255)
+    ubicacion = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    estado = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
+
+# Relé
+class RegistrosRele(models.Model):
+    cod_registro = models.AutoField(primary_key=True)
+    cod_modulo = models.ForeignKey(Modulo, on_delete=models.CASCADE)
+    fecha_hora = models.DateTimeField()
+    accion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'Registro {self.cod_registro}'
+
+#Sensor de flujo de agua
+class RegistrosAgua(models.Model):
+    cod_registro = models.AutoField(primary_key=True)
+    cod_modulo = models.ForeignKey(Modulo, on_delete=models.CASCADE)
+    fecha_hora = models.DateTimeField()
+    flujo = models.DecimalField(max_digits=10, decimal_places=2)
+    unidadmedida = models.CharField(max_length=5)
+    nivelflujo = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'Registro de Agua {self.cod_registro}'
+
+#Sensor ultrasónico
+class RegistrosUltrasonico(models.Model):
+    cod_registro = models.AutoField(primary_key=True)
+    cod_modulo = models.ForeignKey(Modulo, on_delete=models.CASCADE)
+    fecha_hora = models.DateTimeField()
+    distancia = models.DecimalField(max_digits=10, decimal_places=2)
+    unidadmedida = models.CharField(max_length=255)
+    detecmov = models.BooleanField()
+
+    def __str__(self):
+        return f'Registro Ultrasonico {self.cod_registro}'
+
+#Sensor de calidad de aire
+class RegistrosAire(models.Model):
+    cod_registro = models.AutoField(primary_key=True)
+    cod_modulo = models.ForeignKey(Modulo, on_delete=models.CASCADE)
+    fecha_hora = models.DateTimeField()
+    calidad_aire = models.CharField(max_length=255) 
+    monoxcarbono = models.DecimalField(max_digits=10, decimal_places=2)
+    amoniaco = models.DecimalField(max_digits=10, decimal_places=2)
+    alerta = models.CharField(max_length=255)  
+    dlargo_plazo = models.DecimalField(max_digits=10, decimal_places=2)
+    cont_ventilacion = models.BooleanField()
+    cot_purificado = models.BooleanField() 
+
+    def __str__(self):
+        return f'Registro de Aire {self.cod_registro}'
+
+#alertas
+class Alertas(models.Model):
+    cod_alerta = models.AutoField(primary_key=True)
+    cod_modulo = models.ForeignKey(Modulo, on_delete=models.CASCADE)
+    cod_registro = models.ForeignKey(RegistrosRele, on_delete=models.CASCADE)
+    fecha_hora = models.DateTimeField()
+    tipo_alerta = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'Alerta {self.cod_alerta}'
+
+#mantenimientos
+class Mantenimientos(models.Model):
+    cod_mant = models.AutoField(primary_key=True)
+    fecha_hora = models.DateTimeField()
+    tipo_mant = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    realizado = models.BooleanField()
+
+    def __str__(self):
+        return f'Mantenimiento {self.cod_mant}'
+
+#temporizadores
+class Temporizadores(models.Model):
+    cod_temp = models.AutoField(primary_key=True)
+    temp_inicio = models.DateTimeField()
+    temp_fin = models.DateTimeField()
+    accion = models.ForeignKey(RegistrosRele, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Temporizador {self.cod_temp}'
