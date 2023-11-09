84785d37b02531a19b74a0070b25eb2f057f48bb
branyerbjr <branyerbjr@gmail.com>
2023-11-09 09:52:29 -0500

    subiendo despliegue v1.1

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Usa la imagen oficial de Python como base
+FROM python:3.8
+
+# Establece el directorio de trabajo en /app
+WORKDIR /app
+
+# Copia el archivo requirements.txt al contenedor
+COPY requirements.txt .
+
+# Instala las dependencias del proyecto
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copia el contenido del directorio actual al contenedor en /app
+COPY . /app
+
+# Expone el puerto 8000 para que pueda ser accedido desde fuera del contenedor
+EXPOSE 8000
+
+# Comando para ejecutar la aplicación cuando se inicia el contenedor
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- iot/urls.py
+++ iot/urls.py
@@ -2,52 +2,52 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.IndexView.as_view(),name='index'),
-    
-    #Modulos - General
-    path('modulos',views.ModsView.as_view(),name='modulos'),
-    path('modulo/<int:pk>',views.ModsDetailView.as_view()),
+    path('', views.IndexView.as_view(), name='index'),
 
-    #Registros
-    #Rele
-    path('releregistros',views.ReleView.as_view(),name='regisrele'),
-    path('releregistro/<int:pk>',views.ReleDetailView.as_view()),
-    
-    #Sensor de flujo de agua
-    path('aguaregistros',views.AguaView.as_view(),name='regisagua'),
-    path('aguaregistro/<int:pk>',views.AguaDetailView.as_view()),
+    # Modulos - General
+    path('modulos', views.ModsView.as_view(), name='modulos'),
+    path('modulo/<int:pk>', views.ModsDetailView.as_view()),
 
-    #Sensor ultrasónico 
-    path('sonicoregistros',views.SonicView.as_view(),name='regisonicos'),
-    path('sonicoregistro/<int:pk>',views.SonicDetailView.as_view()),
+    # Registros
+    # Rele
+    path('releregistros', views.ReleView.as_view(), name='regisrele'),
+    path('releregistro/<int:pk>', views.ReleDetailView.as_view()),
 
-    #Sensor de calidad de aire
-    path('airegistros',views.AireView.as_view(),name='regisaire'),
-    path('airegistro/<int:pk>',views.AireDetailView.as_view()),
+    # Sensor de flujo de agua
+    path('aguaregistros', views.AguaView.as_view(), name='regisagua'),
+    path('aguaregistro/<int:pk>', views.AguaDetailView.as_view()),
+
+    # Sensor ultrasónico
+    path('sonicoregistros', views.SonicView.as_view(), name='regisonicos'),
+    path('sonicoregistro/<int:pk>', views.SonicDetailView.as_view()),
+
+    # Sensor de calidad de aire
+    path('airegistros', views.AireView.as_view(), name='regisaire'),
+    path('airegistro/<int:pk>', views.AireDetailView.as_view()),
 
     # Alertas
     # Alertas Rele
-    path('relealertas',views.AltReleView.as_view(),name='altrele'),
-    path('relealerta/<int:pk>',views.AltReleDetailView.as_view()),
+    path('relealertas', views.AltReleView.as_view(), name='altrele'),
+    path('relealerta/<int:pk>', views.AltReleDetailView.as_view()),
 
     # Alertas Sensor de flujo de agua
-    path('agualertas',views.AltAguaView.as_view(),name='altagua'),
-    path('agualerta/<int:pk>',views.AltAguaDetailView.as_view()),
+    path('agualertas', views.AltAguaView.as_view(), name='altagua'),
+    path('agualerta/<int:pk>', views.AltAguaDetailView.as_view()),
 
     # Alertas Sensor ultrasónico
-    path('sonicoalertas',views.AltSonicView.as_view(),name='altsonico'),
-    path('sonicoalerta/<int:pk>',views.AltSonicDetailView.as_view()),
+    path('sonicoalertas', views.AltSonicView.as_view(), name='altsonico'),
+    path('sonicoalerta/<int:pk>', views.AltSonicDetailView.as_view()),
 
     # Alertas Sensor de calidad de aire
-    path('airealertas',views.AltAireView.as_view(),name='altaire'),
-    path('airealerta/<int:pk>',views.AltAireDetailView.as_view()),
+    path('airealertas', views.AltAireView.as_view(), name='altaire'),
+    path('airealerta/<int:pk>', views.AltAireDetailView.as_view()),
 
     # mantenimientos
-    path('mantenimientos',views.MantemimientoView.as_view(),name='mantenimientos'),
-    path('mantenimiento/<int:pk>',views.MantemimientoDetailView.as_view()),
+    path('mantenimientos', views.MantemimientoView.as_view(), name='mantenimientos'),
+    path('mantenimiento/<int:pk>', views.MantemimientoDetailView.as_view()),
 
     # temporizadores
-    path('temporizadores',views.TemporizadoresView.as_view(),name='temporizadores'),
-    path('temporizador/<int:pk>',views.TemporizadoresDetailView.as_view()),
+    path('temporizadores', views.TemporizadoresView.as_view(), name='temporizadores'),
+    path('temporizador/<int:pk>', views.TemporizadoresDetailView.as_view()),
 
 ]
