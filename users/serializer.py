2bc03c0514a19847957794922ecc126c93fb1ca3
anyelinash <anyelina.sosa@tecsup.edu.pe>
2023-11-21 09:56:07 -0500

    serializer modificado

--- users/serializer.py
+++ users/serializer.py
@@ -5,13 +5,13 @@ from .models import Empresa, Usuario, Reporte
 class EmpresaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Empresa
-        fields = ('codigo_emp', 'nombre', 'ruc', 'correo', 'contrasena')
+        fields = ('codigo_emp', 'codigo_usu', 'nombre', 'ruc', 'correo', 'contrasena')
 
 
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = ('codigo_usu', 'codigo_emp', 'provider_id', 'provider_specific_uid','nombre', 'dni', 'correo', 'contrasena', 'photo_url')
+        fields = ('codigo_usu', 'provider_id', 'provider_specific_uid','nombre', 'dni', 'correo', 'contrasena', 'photo_url')
         
 
 # Reportes de usuarios
